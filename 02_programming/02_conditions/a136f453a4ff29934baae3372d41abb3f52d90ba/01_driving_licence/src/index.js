b346d9099de5f3d4d82a24d6e481e7e676d683ea
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-01-12 11:32:47 +0100

    exercices mercredi matin

---
+++ 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/01_driving_licence/src/index.js
@@ -0,0 +1,8 @@
+let age = 18;
+let hasDrivingLicence = true;
+
+// Code here a conditional statement that will log "You're not allowed to drive yet." when
+// the age is below 18 or the person doesn't have a driving licence and logs
+if (age<18 && hasDrivingLicence ) {console.log("You're not allowed to drive yet.");else {console.log("the age is below 18 or the person doesn't have a driving licence and logs");
+
+}

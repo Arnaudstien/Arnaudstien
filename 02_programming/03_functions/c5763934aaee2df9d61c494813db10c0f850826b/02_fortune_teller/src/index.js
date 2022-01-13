11abde6066cb0114cfc87df4f8f359275f9b13cf
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-01-13 17:00:05 +0100

    exercice 3

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/02_fortune_teller/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/02_fortune_teller/src/index.js
@@ -1,6 +1,7 @@
-function tellFortune() {
-  // Code the function here.
+function tellFortune(job,y,z,n) {
+  console.log(`You will be ${job} in ${y}, and married to ${z} in ${n} years.`);
 }
+//tellFortune("dev","LILLE","XXXX",2);
 
 // Do not remove last lines, it is for tests
 module.exports = tellFortune;
--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/03_odd_even/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/03_odd_even/src/index.js
@@ -1,6 +1,14 @@
-function oddEven() {
-  // Code the function here.
+function oddEven(number) {
+  if (Number.isInteger(number)){
+    return (number % 2 === 1 ? `${number} is an odd number.` : `${number} is an even number.`);
+
+  } else {
+    return console.log(`${number} is not a number.`);
+  }
 }
 
+
+oddEven(2);
+oddEven("ldldflflflglgg");
 // Do not remove last lines, it is for tests
 module.exports = oddEven;

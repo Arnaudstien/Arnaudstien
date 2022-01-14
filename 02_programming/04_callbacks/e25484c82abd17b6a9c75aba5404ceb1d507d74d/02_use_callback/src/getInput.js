c360c5813fec06e7ed167c14dd168c6d45447ab6
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-01-14 14:35:06 +0100

    exerceice 2

--- 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/02_use_callback/src/getInput.js
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/02_use_callback/src/getInput.js
@@ -1,5 +1,9 @@
 // A function getInput that takes two parameters, an input (the first one) and a callback function
 
-const getInput = // code the function here
-  // Leave the line below for tests to work
-  (module.exports = getInput);
+function getInput(getInput, callback) {
+
+  return console.log(getInput.key);
+};
+
+// Leave the line below for tests to work
+(module.exports = getInput);

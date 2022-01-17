c5b6e6cb2ff63ee4426884ac440427b318523a9d
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-01-17 14:33:54 +0100

    exercice numero 2

--- 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/02_filtering_letter_e/src/index.js
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/02_filtering_letter_e/src/index.js
@@ -1,6 +1,27 @@
-function theEFilter(sentences) {
+//const sentences = ["without using the","oe that, it would have beEn"];
 
+function theEFilter(sentences) {
+  const arrayFilted = sentences.filter((phrase) => {
+    return phrase.includes("e")||phrase.includes("E");
+
+  }
+
+  );
+  return arrayFilted;
 }
+// if (sentences.includes("e") || sentences.includes("E")) {//  return sentences;
+// console.log(sentences);
+//}
+
+//}
+//sentences.filter(theEFilter);
+
+
+
+
+//sentences.includes("e") || sentences.includes("E")? console.log(sentences):false;
+//sentences.filter(theEFilter);
+
 
 // Do not remove the following line, it is for tests
 module.exports = theEFilter;

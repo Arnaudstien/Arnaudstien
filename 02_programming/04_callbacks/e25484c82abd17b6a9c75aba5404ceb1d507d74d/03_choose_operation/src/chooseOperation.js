f245312772a27b299621f3ef417572491caa10e1
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-01-17 10:04:56 +0100

    exo 3

--- 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/03_choose_operation/src/chooseOperation.js
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/03_choose_operation/src/chooseOperation.js
@@ -1,22 +1,38 @@
-function addition() {
-  // Code here
+function addition(a, b,c) {
+
+  const result = c (a+ b);
+
 }
 
-function substraction() {
-  // Code here
+function substraction(a,b,c) {
+
+  const result = c (a-b);
 }
 
-function multiplication() {
-  // Code here
+function multiplication(a,b,c) {
+
+  const result =c (a*b);
 }
 
-function division() {
-  // Code here
+function division(a,b,c) {
+  const result = c (a/b);
 }
 
-function chooseOperation() {
-  // Code here
+function chooseOperation(a,b,c) {
+  //const result = chooseOperation(a,b,c);
+  c(a,b);
 }
+chooseOperation(1,3,addition);
+//function computeAndLog(a, b, callback) {
+//  const result = callback(a, b);
+//  console.log("The result of your operation is ", result);
+//}
+
+//computeAndLog(1, 1, (a, b) => a + b);
 
 // Leave the line below for tests to work
+
+
+
+
 module.exports = { chooseOperation, addition, substraction, multiplication, division };

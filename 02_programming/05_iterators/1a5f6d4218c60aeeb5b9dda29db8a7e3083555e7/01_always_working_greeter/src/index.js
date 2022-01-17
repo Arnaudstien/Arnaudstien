169e4d32a912b5dbb8c3bce494f0d300c453f82b
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-01-17 11:42:48 +0100

    exercice numero 1

---
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/01_always_working_greeter/src/index.js
@@ -0,0 +1,36 @@
+//persons = ["Joe", "Mike", "Robert", "Jose"];
+function greeter(persons) {
+  persons.forEach(name => console.log(`Hello ${name}!`));
+  //console.log(`Hello ${people}!`);
+}
+
+
+
+
+
+
+
+
+//array1.forEach(element => console.log(element));
+
+
+
+
+//function greetOnePerson(person) {
+// console.log(`Hello ${person}`);
+//
+
+//function greet(listOfPeople) {
+// listOfPeople.forEach(greetOnePerson);
+//}
+
+//greet(["Joe", "Mike", "Robert", "Jose"]);
+// "Hello Joe"
+// "Hello Mike"
+// "Hello Robert"
+// "Hello Jose"
+
+// Do not remove the following lines, it is for tests
+module.exports = greeter;
+
+

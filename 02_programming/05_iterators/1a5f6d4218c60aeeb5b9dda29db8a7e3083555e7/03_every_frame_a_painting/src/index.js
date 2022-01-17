eaa6436157df9b7a389384ac1ce25f0ac232c3c3
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-01-17 17:59:39 +0100

    exercice 3

--- 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/03_every_frame_a_painting/src/index.js
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/03_every_frame_a_painting/src/index.js
@@ -1,6 +1,20 @@
-function canTakeArtPieces(pieces) {
+
+
+
+function canTakeArtPieces(pieces){
+
+  return pieces.every(elem => elem.type === "painting" );
 
 }
+
+//canTakeArtPieces();
+
+//[{a:1, b:2}, {a:1, b:3}].every(elem => elem.a === 1); // true
+//[12, 5, 8, 130, 44].every(elem => elem >= 10); // false
+//function greet(listOfPeople) {
+//  listOfPeople.forEach((person) => console.log(`hello ${person}`));
+//
+
 
 // Do not remove the following line, it is for tests
 module.exports = canTakeArtPieces;

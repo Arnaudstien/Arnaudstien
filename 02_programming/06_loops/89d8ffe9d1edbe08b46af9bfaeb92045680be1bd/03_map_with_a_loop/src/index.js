73b5114a311a9ee715572356f8354c907b884f7d
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-01-18 16:59:18 +0100

    exercice numero 3

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/03_map_with_a_loop/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/03_map_with_a_loop/src/index.js
@@ -1,7 +1,29 @@
-function mapLoop() {
-  // Code the function here.
+const array = [1, 2, 3, 4];
 
+function mapLoop(array, arrayMap) {
+  // console.log("array ", array);
+  const arrayResult = [];
+
+  for (let i = 0; i < array.length; i++) {
+    // console.log(arrayMap(array[i]));
+    arrayResult.push(arrayMap(array[i]));
+    //  console.log(arrayResult);
+  }
+  //console.log(arrayResult);
+  return arrayResult;
+  // const double = array.map(arrayMap);
+
+  // const double = arrayMap(array);
+  // console.log(" double", double);
+  //return double;
 }
 
+//mapLoop(array, (x) => x * 2);
+
+//const someSports = ["Biking", "Running", "Skydiving", "Tennis"];
+
+//for (let i = 0; i < someSports.length; i++) {
+//  console.log(someSports[i]);
+//var total = sports.push("football", "tennis");
 // Do not remove the following lines, it is for tests
 module.exports = mapLoop;

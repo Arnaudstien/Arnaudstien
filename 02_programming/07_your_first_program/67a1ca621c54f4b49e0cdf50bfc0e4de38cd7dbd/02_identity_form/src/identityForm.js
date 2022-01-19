4533200182e7cd7bbe5192ca39456a7237573077
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-01-19 11:28:53 +0100

    exercice numero 2

--- 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/02_identity_form/src/identityForm.js
+++ 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/02_identity_form/src/identityForm.js
@@ -1,6 +1,17 @@
 const identityForm = (reader) => {
-  // Code here
+  reader.question("What is your first name? ", (firstname) => {
+    reader.question("What is your last name ? ", (name) => {
+      reader.question("age ? ", (age) => {
+        console.log(`Your name is ${firstname} ${name} and you are ${age}.`);
+        reader.close();
+      });
+    });
+  });
 };
 
+//   console.log(`Hello ${name}!`);
+
+//   reader.close();
+// });
 // Leave line below for tests to work
 module.exports = identityForm;

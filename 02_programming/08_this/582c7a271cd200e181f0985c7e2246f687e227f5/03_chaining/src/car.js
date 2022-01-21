ce7b0a6dec4a9da6b9322a07c5143d137eaac22b
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-01-21 09:44:21 +0100

    exercice 3

--- 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/03_chaining/src/car.js
+++ 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/03_chaining/src/car.js
@@ -1,9 +1,37 @@
 // Complete and add needed car properties:
 const car = {
-  speed: 0,
+  vitesse: 0,
+  kmheurespeed: 0,
+  minutes: 0,
   start: function () {
-  
+    //  return this.car.speed;
+  },
+  changeSpeed: function (speed) {
+    //this.vitesse = car.changeSpeed(speed);
+    console.log("vitesse"((this.vitesse = `${speed}`)));
+    // return (this.vitesse = `${speed}`);
+    return this;
+  },
+  drive: function (minutes) {
+    //console.log("minutes"((this.vitesse = `${minutes}`)));
+    console.log(""((this.vitesse = `${minutes}` + 100)));
+    console.log(`hello ${minutes}!`);
+    return this;
+  },
+  showDistance: function () {
+    console.log((this.kmheurespeed = this.vitesse + this.minutes));
+    // console.log(`hello ${""}!`);
+    return this;
   },
 };
+
+console.log(car.changeSpeed(130).drive(40).showDistance());
+//console.log(car.speed);
+//console.log((car.kmheurespeed = 130));
+//console.log(car.start());
+//console.log(car.start().changeSpeed(130).drive(42).showDistance());
+//car.start().changeSpeed(130).drive(42).showDistance();
+
+//console.log(car.start()); //.changeSpeed(130).drive(42).showDistance());
 
 module.exports = car;

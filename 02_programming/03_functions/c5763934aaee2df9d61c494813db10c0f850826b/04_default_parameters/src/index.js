bc6e14f70e578d5d69367d23ba118af71ae0ce34
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-01-25 17:59:00 +0100

    exercice 4

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/04_default_parameters/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/04_default_parameters/src/index.js
@@ -1,6 +1,22 @@
-function helloSpartan() {
-  // Code the function here.
+//function helloSpartan(string) {
+// console.log(`Hello ${string}: glad to know your name!`);
+//  return `Hello ${name}: glad to know your name!`;
+// Code the function here.
+//}
+//helloSpartan("John");
+
+//helloSpartan("soldier");
+
+//console.log(helloSpartan);
+// Do not remove last lines, it is for tests
+
+function helloSpartan(params = "John", param2) {
+  console.log(`Hello ${params}: glad to know your name!`);
+  console.log(`Hello ${param2}: not very talkative uh?`);
 }
 
-// Do not remove last lines, it is for tests
+helloSpartan();
+
+helloSpartan(param1, "soldier");
+
 module.exports = helloSpartan;

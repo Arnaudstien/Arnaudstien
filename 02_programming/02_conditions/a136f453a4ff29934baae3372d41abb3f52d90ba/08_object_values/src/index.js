dc600e53de8938619044ac3b48c953f219eb05a0
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-01-25 15:13:08 +0100

    exercice 8

--- 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/08_object_values/src/index.js
+++ 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/08_object_values/src/index.js
@@ -12,13 +12,21 @@ const odinRequest = "Mjöllnir";
 // Code here a constant armouryWithNamesOnly bound to an array called that has all the values of the object armoury
 // using the object method to get all values (google is your friend)
 
+const armouryWithNamesOnly = [armoury.spear, armoury.hammer, armoury.chain, armoury.sword, armoury.shoes, armoury.horn];
 
+console.log(armouryWithNamesOnly);
 
+//const armouryWithNamesOnly = [armoury.spear + armoury.hammer];
+//console.log(armouryWithNamesOnly);
+const isTheRequestInArmoury = armouryWithNamesOnly.includes(odinRequest);
+console.log(isTheRequestInArmoury);
 // Create a constant isTheRequestInArmoury bound to the check of the presence
 // of odinRequest in armouryWithNamesOnly
 
-
-
 // Create a condition that logs "Ah, I finally found something to hammer that nail into"
 // if the isTheRequestInArmoury is true and "What do you want me to do with this?" if not
-
+if (isTheRequestInArmoury) {
+  console.log("Ah, I finally found something to hammer that nail into");
+} else {
+  console.log("What do you want me to do with this?");
+}

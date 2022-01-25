55b0297efb9c5a111455c1ac9d61bae245221d5d
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-01-25 11:47:51 +0100

    exercice 6

--- 02_programming/01_variables/969830e3c3dda891a6b1aeed2709911232022bb1/06_play_with_charat/src/index.js
+++ 02_programming/01_variables/969830e3c3dda891a6b1aeed2709911232022bb1/06_play_with_charat/src/index.js
@@ -3,7 +3,18 @@ const question = "spartan, what is your profession?!";
 // a constant `letterAtThirdPosition` bound to a string that is the third position character in question
 // using `charAt()` and the previous constant `question`
 
-
+const letterAtThirdPosition = question.charAt(2);
+console.log(letterAtThirdPosition);
 // a constant `letterAtFourthPosition` bound to a string: "The letter at the fourth position is r"
 // using `charAt()` and the previous constant `question`
 
+// eslint-disable-next-line no-template-curly-in-string
+//const questioné = question.charAt(4);
+const letterAtFourthPosition = `The letter at the fourth position is ${question.charAt(3)}`;
+
+//const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+//const index = 4;
+
+//console.log(`The character at index ${index} is ${sentencen.charAt(idex)}`);
+// expected output: "The character at index 4 is q"

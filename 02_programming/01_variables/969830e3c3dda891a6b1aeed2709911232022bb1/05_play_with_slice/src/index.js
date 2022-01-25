5ca479733564b096a858532500645ab1557081bf
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-01-25 11:29:16 +0100

    exercice 5

--- 02_programming/01_variables/969830e3c3dda891a6b1aeed2709911232022bb1/05_play_with_slice/src/index.js
+++ 02_programming/01_variables/969830e3c3dda891a6b1aeed2709911232022bb1/05_play_with_slice/src/index.js
@@ -1,8 +1,11 @@
-const question = "spartan, what is your profession?!";
-
 // a constant `hobbyQuestion` bound to a string: "spartan, what is your hobby?!"
+//const question = "hobby";
+const question = "spartan, what is your profession?!";
+const hobbyQuestion = question.slice(0, 21) + " hobby?!";
+console.log(hobbyQuestion);
 // using `slice()` and the constant `question`
-const hobbyQuestion = question.slice(hobbyQuestion);
-
-// a constant `hobbyQuestionLength` bound to a number that is the length of `hobby`
+//console.log(question.slice(0, 4).length);
 const hobbyQuestionLength = hobbyQuestion.length;
+//console.log(question.length.slice(0, 4));
+console.log(hobbyQuestionLength);
+console.log(question.length);

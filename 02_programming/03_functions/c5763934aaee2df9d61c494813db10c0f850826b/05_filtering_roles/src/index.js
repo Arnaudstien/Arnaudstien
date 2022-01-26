38ef86ba0d9bd98c572614bcd1219fa55f9dc48e
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-01-26 15:39:12 +0100

    exercice 3

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/05_filtering_roles/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/05_filtering_roles/src/index.js
@@ -1,7 +1,40 @@
-function filteredRoles(){
-  // Code the function here.
+function filteredRoles(personarray, roles = "Teacher") {
+  console.log(roles);
+  if (roles === "Teacher") {
+  }
 
+  //console.log(personarray[0].role);
+  //  console.log(personarray[1].role);
+
+  // console.log(personarray[0].role);
+  // console.log(roles);
+  // console.log(personarray[0], personarray[1]);
 }
+const person2 = [
+  {
+    name: "Frieda",
+    role: "Teacher",
+  },
+  {
+    name: "John",
+    role: "Student",
+  },
+];
+//const person = ["Frieda", "Teacher"];
 
-// Do not remove lines below, it is for tests
+filteredRoles(person2, "Teacher");
+//filteredRoles(person2);
+
+//filteredRoles(person);
+
+//function myFunction2(array){
+// array[0]; // qui donnera "Arnaud"
+//  ...
+//}
+
+//const persons = ["Arnaud", "Florian"];
+//myFunction2(persons);
+
+//filteredRoles(personarray[0].roles);
+
 module.exports = filteredRoles;
--- 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/03_choose_operation/src/chooseOperation.js
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/03_choose_operation/src/chooseOperation.js
@@ -1,38 +1,26 @@
-function addition(a, b,c) {
-
-  const result = c (a+ b);
-
+function addition(a, b) {
+  return a + b;
 }
 
-function substraction(a,b,c) {
-
-  const result = c (a-b);
+function substraction(a, b) {
+  return a - b;
 }
 
-function multiplication(a,b,c) {
-
-  const result =c (a*b);
+function multiplication(a, b) {
+  return a * b;
 }
 
-function division(a,b,c) {
-  const result = c (a/b);
+function division(a, b) {
+  return a / b;
 }
 
-function chooseOperation(a,b,c) {
-  //const result = chooseOperation(a,b,c);
-  c(a,b);
+function chooseOperation(a, b, c) {
+  if (Number.isInteger(a) && Number.isInteger(b)) {
+    return c(a, b);
+  } else {
+    throw new Error("Superior at 10");
+  }
 }
-chooseOperation(1,3,addition);
-//function computeAndLog(a, b, callback) {
-//  const result = callback(a, b);
-//  console.log("The result of your operation is ", result);
-//}
-
-//computeAndLog(1, 1, (a, b) => a + b);
-
-// Leave the line below for tests to work
-
-
-
+chooseOperation(1, 3, addition);
 
 module.exports = { chooseOperation, addition, substraction, multiplication, division };

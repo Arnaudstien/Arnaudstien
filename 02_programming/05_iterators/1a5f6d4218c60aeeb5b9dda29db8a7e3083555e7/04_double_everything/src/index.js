c57300fe3e54011576dc83f521ee1a8672ec4481
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-01-27 13:48:54 +0100

    exercice 4

--- 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/04_double_everything/src/index.js
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/04_double_everything/src/index.js
@@ -1,6 +1,7 @@
 function double(numbers) {
-
+  return numbers.map((numbers) => numbers * 2);
 }
 
+//console.log(doubledDigits); // => [2, 4, 6]
 // Do not remove the following line, it is for tests
 module.exports = double;

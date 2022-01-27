20dd2b897f9af3a8fe6631b7191145aad3a57768
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-01-27 15:11:21 +0100

    exercice 4

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/04_99_bottles_of_beer/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/04_99_bottles_of_beer/src/index.js
@@ -1,6 +1,26 @@
-function sing () {
-  // code here
-};
+function sing() {
+  const music = [];
+  for (let i = 99; i >= 0; i--) {
+    if (i === 2) {
+      music.push("2 bottles of beer on the wall, 2 bottles of beer.");
+      music.push("Take one down and pass it around, 1 bottle of beer on the wall.");
+    } else if (i === 1) {
+      music.push("1 bottle of beer on the wall, 1 bottle of beer.");
+      music.push("Take one down and pass it around, no more bottles of beer on the wall.");
+    } else if (i === 0) {
+      music.push("No more bottles of beer on the wall, no more bottles of beer.");
+      music.push("Go to the store and buy some more, 99 bottles of beer on the wall.");
+    } else {
+      music.push(`${i} bottles of beer on the wall, ${i} bottles of beer.`);
+      music.push(`Take one down and pass it around, ${i - 1} bottles of beer on the wall.`);
+    }
+  }
+  return music;
+}
+sing();
+//const song = sing();
+//console.log(song);
 
-// Do not remove the following line, it is for tests
+// code here
+
 module.exports = sing;

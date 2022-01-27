ecd91b8dc98ebec3bdcebce655ef0dcbf4528e36
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-01-27 11:02:56 +0100

    exercice

--- 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/06_recursion/src/recursion.js
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/06_recursion/src/recursion.js
@@ -1,4 +1,13 @@
-const findPosition = () => {
+const findPosition = (list, persons, counter) => {
+  if (Array.isArray(list) && typeof list[0] === "string" && typeof persons === "string" && Number.isInteger(counter)) {
+    if (list.includes(persons)) {
+      return `${persons} is at position #${list.indexOf(persons) + 1} in this array.`;
+    } else {
+      return `${persons} is not present in this array.`;
+    }
+  } else {
+    throw new Error("A parameter is not of its expected type ");
+  }
   // Code here
 };
 

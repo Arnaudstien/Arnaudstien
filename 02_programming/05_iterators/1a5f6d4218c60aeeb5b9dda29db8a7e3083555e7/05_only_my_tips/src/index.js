f8071ddc203014fbebc9cf14f9ad8d80b87f62d2
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-01-28 08:54:26 +0100

    exercice  5

--- 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/05_only_my_tips/src/index.js
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/05_only_my_tips/src/index.js
@@ -1,12 +1,31 @@
 function onlyMyTips(invoices) {
-  // Code here
+  if (invoices.filter((invoices) => invoices === "Yourself")) {
+    console.log(invoices.filter((invoices) => invoices === "Yourself"));
+  }
 }
+//return invoices.filter((invoices) => invoices === "Yourself");
+//console.log(invoices);
+
+//const result = words.filter(word => word.length > 6);
+
+//return todaysInvoices.map([0].tip, todaysInvoices[1].tip, todaysInvoices[2].tip);
+//todaysInvoices[0].tip, todaysInvoices[1].tip, todaysInvoices[2].tip; // Code here
 
 const todaysInvoices = [
   { clients: 3, waiter: "Yourself", price: 5100, tip: 200 },
   { clients: 2, waiter: "Dominique", price: 2200, tip: 50 },
   { clients: 2, waiter: "Yourself", price: 2900, tip: 100 },
 ];
+onlyMyTips(todaysInvoices);
+
+//console.log(todaysInvoices[0].tip, todaysInvoices[1].tip, todaysInvoices[2].tip);
+
+//function doubleOddNumbers(numbers) {
+//  return numbers.filter((number) => number % 2 === 1).map((oddNumber) => oddNumber * 2);
+//}
+
+//doubleOddNumbers([1, 2, 3, 4, 5]);
+// -> [2, 6, 10]
 
 // Do not remove the following line, it is for tests
 module.exports = onlyMyTips;

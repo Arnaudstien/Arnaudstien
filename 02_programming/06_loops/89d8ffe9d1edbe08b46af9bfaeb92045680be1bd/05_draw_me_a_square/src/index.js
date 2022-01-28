ab71e57673d0fbd289ab7f39af0fa829beda4617
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-01-28 08:56:05 +0100

    exercice 5

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/05_draw_me_a_square/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/05_draw_me_a_square/src/index.js
@@ -1,5 +1,12 @@
-function square() {
-  // Code here
+function square(size) {
+  let croix = "";
+
+  for (let i = 0; i < size; i++) {
+    croix += "*";
+  }
+  for (let i = 0; i < size; i++) {
+    console.log(croix);
+  }
 }
 
 // Do not remove the following line, it is for tests

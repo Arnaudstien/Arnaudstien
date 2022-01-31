a126c2db4d4b161785b2d6c637538ffa2e8fd80e
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-01-31 15:36:59 +0100

    exercice 3

--- 02_programming/15_typescript_101/f284fbcc79bce170e7c536c6c6a19b31653e10bb/03_typed_function_with_type_alias/src/index.ts
+++ 02_programming/15_typescript_101/f284fbcc79bce170e7c536c6c6a19b31653e10bb/03_typed_function_with_type_alias/src/index.ts
@@ -1,6 +1,19 @@
-function handleTuple() {
-  // Code the function here.
+type MyTuple = [string, number];
+
+function handleTuple(parameter: MyTuple): void {
+  if (typeof parameter[0] === "string") {
+    console.log(`${parameter[0]} is a string`);
+  }
+  if (typeof parameter[1] === "number") {
+    console.log(`${parameter[1]} is a number`);
+  }
+
+  //Code the function here.
 }
+
+//const data2: Data = ["foo", 42];
+
+handleTuple(["foo", 42]);
 
 // Leave the line below for tests to work properly.
 export { handleTuple };

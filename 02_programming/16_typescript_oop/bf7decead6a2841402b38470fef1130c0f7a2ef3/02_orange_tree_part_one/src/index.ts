e94ea9718816c8dde37b8d0612e3bfd63c4f2737
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-02-02 08:56:58 +0100

    exercice 2

--- 02_programming/16_typescript_oop/bf7decead6a2841402b38470fef1130c0f7a2ef3/02_orange_tree_part_one/src/index.ts
+++ 02_programming/16_typescript_oop/bf7decead6a2841402b38470fef1130c0f7a2ef3/02_orange_tree_part_one/src/index.ts
@@ -1,4 +1,34 @@
-// Code the class here.
+class Tree {
+  // Properties member.
+  age: number;
+  height: number;
+  alive: boolean;
 
-// Leave the line below for tests to work properly.
+  // Constructor member.
+  constructor(age: number) {
+    this.age = age;
+  }
+  // Methods member.
+  calc(size: number): void {
+    this.height = 0;
+
+    if (size >= 1 && size <= 9) {
+      this.height = size * 25;
+      console.log("+++++++++1111++++++++", this.height);
+    }
+    if (size > 9 && size <= 20) {
+      this.height = 9 * 25 + (size - 9) * 10;
+      console.log("+++++++++2++++++++", this.height);
+    }
+    if (size > 20) {
+      this.height = 225 + 110;
+      console.log("+++++++++3++++++++", this.height);
+    }
+  }
+}
+
+const tree = new Tree(9);
+
+tree.calc(19);
+
 export { Tree };

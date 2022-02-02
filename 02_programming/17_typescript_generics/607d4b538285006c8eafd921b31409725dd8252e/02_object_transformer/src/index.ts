efd067ca6ed7fbf8b0f91e592463006e0fdde011
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-02-02 17:13:55 +0100

    exercice 2

--- 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/02_object_transformer/src/index.ts
+++ 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/02_object_transformer/src/index.ts
@@ -1,3 +1,28 @@
+type Product = {
+  productName: string;
+  quantity: number;
+};
 
+//function productTransformer(params: Product): (string | number)[][] {
+
+function productTransformer(params: Product): [string, string | number][] {
+  //function productTransformer(params: Product): [string, (string | number)] {
+  [
+    ["productName", "Arnaud"],
+    ["quantity", 43],
+  ];
+  //const array = Object.entries;
+  //console.log(array);
+  //return new Map(Object.entries(params));
+  // console.log(Object.entries(params));
+
+  return Object.entries(params);
+}
+
+// const testObject = {
+//   productName: "Arnaud",
+//   quantity: 43,
+// };
+// console.log(Object.entries(testObject));
 // Leave the line below for tests to work properly.
 export { productTransformer };

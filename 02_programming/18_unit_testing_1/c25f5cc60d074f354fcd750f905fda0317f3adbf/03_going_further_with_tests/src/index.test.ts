c65c633c23787c6c1f623ffb74b70f023f603e0d
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-02-03 21:05:59 +0100

    exercice 3

--- 02_programming/18_unit_testing_1/c25f5cc60d074f354fcd750f905fda0317f3adbf/03_going_further_with_tests/src/index.test.ts
+++ 02_programming/18_unit_testing_1/c25f5cc60d074f354fcd750f905fda0317f3adbf/03_going_further_with_tests/src/index.test.ts
@@ -7,10 +7,9 @@ beforeEach(() => {
 });
 
 describe("Let the tests on arrays begin !", () => {
-
-  // The following arrays are a short list of "must watch" movies. 
+  // The following arrays are a short list of "must watch" movies.
   // Write a test for each of them, respecting the conditions written in the requirements.
-  // Be sure to always test if you test passes with the right conditions but also if 
+  // Be sure to always test if you test passes with the right conditions but also if
   // it does fail with the wrong conditions. Don't forget `expect.assertions`!
 
   // Here's an example:
@@ -20,27 +19,57 @@ describe("Let the tests on arrays begin !", () => {
     mockArray = ["Into the wild", "Alien the 8th passenger"];
 
     expect(mockArray.length).toBe(2);
-  })
+  });
 
   // Create a test that ensures that "Alien the 8th passenger" is present in mockArray.
   test("Alien the 8th passenger is present in mockArray", () => {
     expect.assertions(1);
+    mockArray = ["Into the wild", "Alien the 8th passenger", "Ip Man", "Tombeau des lucioles", "fight club"];
+    //console.log(mockArray.includes("Alien the 8th passenger"));
+    expect(mockArray.includes("Alien the 8th passenger")).toEqual(true);
+  });
 
-    mockArray = ["Into the wild", "Alien the 8th passenger","Ip Man","Tombeau des lucioles", "fight club"];
-  })
+  test("Indiana Jones 4 is not present in mockArray", () => {
+    expect.assertions(1);
+    // Create a test that fails if "Indiana Jones 4" is present in mockArray.
+    mockArray = ["Into the wild", "Alien the 8th passenger", "Ip Man", "Tombeau des lucioles", "fight club"];
+    //console.log(mockArray.includes("Indiana Jones 4"));
+    expect(mockArray.includes("Indiana Jones 4")).toBe(false);
+  });
 
-  // Create a test that fails if "Indiana Jones 4" is present in mockArray.
-  mockArray = ["Into the wild", "Alien the 8th passenger","Ip Man","Tombeau des lucioles", "fight club"];
-
-  // Create a test fails if the list has less than 5 elements.
-  mockArray = ["Lord of the rings : the return of the king", "Into the wild", "Alien the 8th passenger", "Tombeau des lucioles", "fight club"];
+  test("The list as less than 5 elements", () => {
+    expect.assertions(1);
+    // Create a test fails if the list has less than 5 elements.
+    mockArray = [
+      "Lord of the rings : the return of the king",
+      "Into the wild",
+      "Alien the 8th passenger",
+      "Tombeau des lucioles",
+      "fight club",
+    ];
+    //console.log(mockArray.length <s5);
+    expect(mockArray.length < 5).toBe(false);
+  });
 
   // Create a test that checks if the array only contains String element.
   // Remember that a test is code, you are not limited to using `expect`.
-  mockArray = ["Lord of the rings : the return of the king", "Into the wild", "Alien the 8th passenger", "Tombeau des lucioles", "fight club"];
+  mockArray = [
+    "Lord of the rings : the return of the king",
+    "Into the wild",
+    "Alien the 8th passenger",
+    "Tombeau des lucioles",
+    "fight club",
+  ];
+  for (let i = 0; i <= mockArray.values.length; i++) console.log("=======================", i);
 
   // Create a test that passes if there's at least one film with "Lord of the rings" inside mockArray.
-  mockArray = ["Lord of the rings : the return of the king", "Into the wild", "Alien the 8th passenger", "Tombeau des lucioles", "fight club"];
+  mockArray = [
+    "Lord of the rings : the return of the king",
+    "Into the wild",
+    "Alien the 8th passenger",
+    "Tombeau des lucioles",
+    "fight club",
+  ];
 
   //Create a test that checks if the elements in mockArray respect that the strings are the same with the right order as solutionArray
   //eg : mockArray = ["Blue","Blue","Red", "Yellow"] would fail the test
@@ -49,7 +78,6 @@ describe("Let the tests on arrays begin !", () => {
 });
 
 describe("Let the tests on objects begin !", () => {
-
   // Create a test that ensures that your 'command' object contains at least `fries: true` and `sauce: "Hannibal"` without checking properties one by one
   // You should do it with only one assertion.
   // Hint: we want our mock object to _match_ another smaller object (look at the documentation!)

e1503ccea452a44ec4acabfd7e25805494fb21ea
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-02-03 09:36:34 +0100

    exercice 3

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/06_draw_me_a_triangle/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/06_draw_me_a_triangle/src/index.js
@@ -1,14 +1,10 @@
-function triangle(array) {
-  let croix = "";
-  for (let i = 0; array.length; i++) {
-    croix = "*";
-    console.log(croix);
-  }
-
-  for (let i = 0; i < array.length; i++) {
-    console.log(array);
+function triangle(params) {
+  let iterable = "*********************";
+  for (let params of iterable) {
+    console.log(params);
   }
 }
 
+triangle(4);
 // Do not remove the following line, it is for tests
 module.exports = triangle;

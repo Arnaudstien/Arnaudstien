3a741ffc941098e9f80e51c289ff774ed107455e
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-02-07 20:43:29 +0100

    exercice 7

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/07_letter_count/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/07_letter_count/src/index.js
@@ -1,6 +1,13 @@
-function letterCount() {
-  // Code here
+function letterCount(chaine, lettre) {
+  //"this is foo bar".split("o").length-1 Code here
+  console.log(chaine);
+  console.log(lettre);
+  return chaine.split(lettre).length - 1;
 }
 
+letterCount("gattaca", "a");
+
+// var chaine = "1 2 6 8 2 1 6 9 8 2 4 2 3 6 2";
+// alert('le nombre 2 est présent ' + (chaine.split("2").length - 1));
 // Do not remove the following line, it is for tests
 module.exports = letterCount;

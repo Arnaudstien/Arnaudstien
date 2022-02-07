78e9f7c04e621918e21abc986e03ae65ec43926d
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-02-07 08:57:04 +0100

    exercice 4

--- 02_programming/18_unit_testing_1/c25f5cc60d074f354fcd750f905fda0317f3adbf/04_coffee_recipes/src/index.ts
+++ 02_programming/18_unit_testing_1/c25f5cc60d074f354fcd750f905fda0317f3adbf/04_coffee_recipes/src/index.ts
@@ -1,3 +1,10 @@
-export function giveRecipe() {
-  //code here
+export function giveRecipe(coffee: string): string {
+  console.log(giveRecipe.length);
+
+  console.log(coffee);
+
+  return "function";
 }
+
+//giveRecipe("latte");
+//giveRecipe("anything else");

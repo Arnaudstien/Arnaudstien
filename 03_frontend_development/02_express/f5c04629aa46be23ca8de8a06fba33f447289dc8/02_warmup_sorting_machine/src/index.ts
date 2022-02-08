d51cbf71c0f3f9d7b989f3bd8cc92c28e100853c
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-02-08 11:29:51 +0100

    exercice 2

--- 03_frontend_development/02_express/f5c04629aa46be23ca8de8a06fba33f447289dc8/02_warmup_sorting_machine/src/index.ts
+++ 03_frontend_development/02_express/f5c04629aa46be23ca8de8a06fba33f447289dc8/02_warmup_sorting_machine/src/index.ts
@@ -1,1 +1,9 @@
 // Code here
+function sortingMachine(number: number[], result: boolean): number[] {
+  if (result === true) {
+    return number.sort((a, b) => a - b);
+  } else {
+    return number.sort((a, b) => b - a);
+  }
+}
+export { sortingMachine };

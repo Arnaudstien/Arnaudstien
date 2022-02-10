513c7574958098076a1285e87a2663dd68e01550
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-02-10 10:29:28 +0100

    exercice 1

---
+++ 03_frontend_development/04_forms_and_cookies/e5b3d8438cc5eff9e68b205b3efb73619e89654c/01_fundamentals_antiVowels_function/src/sentenceJoiner.ts
@@ -0,0 +1,4 @@
+export const sentenceJoiner = (tableau: string[]): string => {
+  return tableau.join(" ");
+  // Code the sentenceJoiner function here
+};
---
+++ 03_frontend_development/04_forms_and_cookies/e5b3d8438cc5eff9e68b205b3efb73619e89654c/01_fundamentals_antiVowels_function/src/sentenceSplitter.ts
@@ -0,0 +1,11 @@
+export const sentenceSplitter = (chaine: string): string[] => {
+  console.log("===========================", chaine);
+
+  const array = chaine.split(" ");
+  console.log(array);
+  const array1 = array.join(" ");
+  console.log("==================", array1);
+  return array;
+  //return array.join(" ");
+  // Code your function sentenceSplitter here
+};

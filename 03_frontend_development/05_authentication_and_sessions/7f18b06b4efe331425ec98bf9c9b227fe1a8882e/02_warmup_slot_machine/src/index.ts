24a0182a33d5863a16eed0cdf9382c7a76160175
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-02-11 14:48:28 +0100

    exercice 2

--- 03_frontend_development/05_authentication_and_sessions/7f18b06b4efe331425ec98bf9c9b227fe1a8882e/02_warmup_slot_machine/src/index.ts
+++ 03_frontend_development/05_authentication_and_sessions/7f18b06b4efe331425ec98bf9c9b227fe1a8882e/02_warmup_slot_machine/src/index.ts
@@ -1,1 +1,19 @@
 // Code here
+const paylineOfStrings = ["🍒", "🍒", "🍒"];
+// const paylineOfNumbers = [1, 1, 1];
+// const losingPayline = [true, false, true];
+
+function slotMachine(valOne: string[]): boolean {
+  console.log("$$$$$$$$$000$$$$$$$$$$$", valOne[0]);
+  console.log("$$$$$$$$$$$$111$$$$$$$$", valOne[1]);
+  console.log("$$$$$$$2222$$$$$$$$$$$$$", valOne[2]);
+  if (valOne[0] === valOne[1] && valOne[1] === valOne[2]) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+slotMachine(paylineOfStrings);
+
+export { slotMachine };

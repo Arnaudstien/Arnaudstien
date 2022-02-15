1cc31c1fb621921658db046abeb7fa54f046b059
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-02-15 13:55:16 +0100

    exo2 filter

--- 04_backend_web_development_and_api/02_APIs/0570c0a978c86da20a524b74e3f45e43f70842eb/02_warmum_filter_by_first_letter_alphabetically/src/index.ts
+++ 04_backend_web_development_and_api/02_APIs/0570c0a978c86da20a524b74e3f45e43f70842eb/02_warmum_filter_by_first_letter_alphabetically/src/index.ts
@@ -1,1 +1,17 @@
 // code here
+import { Customer } from "../data/customers";
+
+export function filterByFirstLetter(custumers: Customer[], letter: string): Customer[] {
+  const cust = custumers;
+  console.log("mfmfmfmfmfm", letter);
+
+  const result = cust.filter((elem) => elem.last_name.charAt(0) === letter);
+  return result;
+}
+export function filterByFirstLetterOrdered(clients: Customer[], letters: string): Customer[] {
+  console.log("clients", clients);
+  console.log("letters", letters);
+  const essais = clients.sort((a, b) => (a.last_name > b.last_name ? 1 : -1));
+  console.log(essais);
+  return filterByFirstLetter(essais, letters);
+}

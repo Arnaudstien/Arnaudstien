6af4c008347acef549fd578861f2db9548e244ce
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-02-15 09:00:15 +0100

    chuck_norris api

--- 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/03_chuck_norris/src/getCategories.ts
+++ 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/03_chuck_norris/src/getCategories.ts
@@ -1,7 +1,15 @@
 import request from "@fewlines-education/request";
 
-function getCategories() {
-  // code the function here
+function getCategories(): void {
+  request("https://api.chucknorris.io/jokes/categories", (error, html) => {
+    if (error) {
+      console.error(error);
+      return error;
+    } else {
+      console.log(html);
+      return html;
+    }
+  });
 }
 
 // leave line below for tests to work properly
--- 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/03_chuck_norris/src/getChuckNorrisJoke.ts
+++ 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/03_chuck_norris/src/getChuckNorrisJoke.ts
@@ -1,7 +1,18 @@
 import request from "@fewlines-education/request";
 
-function getChuckNorrisJoke() {
+function getChuckNorrisJoke(): void {
   // code the function here
+  request("https://api.chucknorris.io/jokes/random?category=dev", (error, body) => {
+    if (error) {
+      console.error(error);
+      return error;
+    } else {
+      console.log(body);
+      const data = JSON.parse(body);
+      console.log("data$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$", data);
+      //return body;
+    }
+  });
 }
 
 // leave line below for tests to work properly
